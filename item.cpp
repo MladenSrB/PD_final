d61a316de1b3258d4334fc7b495be65d86b5ec79
SrBMladen <srbmladen@icloud.com>
2024-12-01 18:29:18 +0800

    item.cpp updated

--- item.cpp
+++ item.cpp
@@ -4,7 +4,7 @@ Item::Item(string aItemType, string aItemName)
 {
     itemName = aItemName;
     itemType = aItemType;
-}
+};
 
 Potion(string itemType, string itemName) : Item(itemType, itemName) { potionType = itemType; };
 void useWeapon(Character &aChar) { aChar.setatk(baseATK); }

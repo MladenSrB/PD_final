3b4cb7883c6c1b51f790525b99f6a309486bb932
Yuhan0818 <yuhan920818@gmail.com>
2024-12-03 00:51:56 +0800

    1203

--- character.h
+++ character.h
@@ -34,7 +34,13 @@ public:
     void upgrade();
     void addItem(Item i);
     bool usedItem(Item i);
+    void addCoin(int c){coin += c;};
+    int getCoin(){return coin;};
+
     void addExp(int pt);
+    void decreaseHp(int blood){hp -= blood;};
+    void addHp(int blood){hp += blood;};
+    int getExp(){ return exp;};
 };
 
 class Monster : public Character
--- dino_game.cpp
+++ dino_game.cpp
@@ -18,7 +18,7 @@ void DinoGame::resetInput() {
     tcsetattr(STDIN_FILENO, TCSANOW, &term);
 }
 
-void DinoGame::displayGame(char map[height][width], int score) {
+void DinoGame::displayGame(char map[height][width], int score, int countdown) {
     system("clear");
     for (int i = 0; i < height; ++i) {
         for (int j = 0; j < width; ++j) {
@@ -27,6 +27,7 @@ void DinoGame::displayGame(char map[height][width], int score) {
         cout << endl;
     }
     cout << "Score: " << score << endl;
+    cout << "Time remaining: " << countdown << " seconds" << endl;
 }
 
 void DinoGame::updateMap(char map[height][width], vector<int> &obstacleX, int &score, int &obstacleSpawnTimer) {
@@ -44,16 +45,21 @@ void DinoGame::updateMap(char map[height][width], vector<int> &obstacleX, int &s
     // 生成新障礙物
     if (obstacleSpawnTimer <= 0) {
         obstacleX.push_back(width - 1);
-        obstacleSpawnTimer = rand() % 15 + 5;
+        obstacleSpawnTimer = rand() % 10 + 3;  // 調整這裡，讓間隔時間更短
     } else {
         obstacleSpawnTimer--;
     }
 
     // 更新障礙物位置和計分
     for (int &x : obstacleX) {
-        if (x == 1 && dinoY == height - 2) {
+        if (x == 1 && dinoY == height - 2) { // 碰撞檢測
             gameOver = true;
         }
+
+        if (x == 1 && dinoY != height - 2) { // 成功避開障礙物
+            score++; 
+        }
+        
         x--;
     }
 
@@ -83,17 +89,38 @@ void DinoGame::jump() {
     isJumping = false;
 }
 
-void DinoGame::runGame() {
+void DinoGame::runGame(Player player) {
     char map[height][width];
     vector<int> obstacleX;
     int obstacleSpawnTimer = 0;
     int score = 0;
+    int coin = 0;
+    int exp = 0;
+    int blood = 10;
 
     srand(time(0));
 
     setNonBlockingInput();
 
+    auto startTime = chrono::steady_clock::now(); // 開始計時
+
     while (!gameOver) {
+        // 檢查時間是否超過15秒
+        auto currentTime = chrono::steady_clock::now();
+        auto elapsedTime = chrono::duration_cast<chrono::seconds>(currentTime - startTime).count();
+        int countdown = 15 - elapsedTime;
+
+        if (countdown <= 0) {
+            coin = score / 3;
+            exp = score / 2;
+            player.addCoin(coin);
+            player.addExp(exp);
+            cout << "Congratulations! You won the game!\n" << endl;
+            cout << "coin +" << coin << ", total coin: " << player.getCoin() << endl;
+            cout << "exp +" << exp << ", total exp: " << player.getExp() << endl;
+            break;
+        }
+
         char ch;
         if (read(STDIN_FILENO, &ch, 1) > 0 && ch == ' ') {
             thread t([this] { jump(); });
@@ -101,12 +128,23 @@ void DinoGame::runGame() {
         }
 
         updateMap(map, obstacleX, score, obstacleSpawnTimer);
-        displayGame(map, score);
+        displayGame(map, score, countdown);
         this_thread::sleep_for(milliseconds(100));
     }
 
+    if (gameOver) {
+        coin = score / 3;
+        exp = score / 2;
+        player.addCoin(coin);
+        player.addExp(exp);
+        player.decreaseHp(blood);
+        cout << "Game Over!" << endl;
+        cout << "coin +" << coin << ", total coin: " << player.getCoin() << endl;
+        cout << "exp +" << exp << ", total exp: " << player.getExp() << endl;
+        cout << "hp -" << blood << ", total hp: " << player.gethp() << endl;
+    }
+
     resetInput();
-    cout << "Game Over! Your score: " << score << endl;
 }
 
 const char DinoGame::ground = '_';
@@ -114,7 +152,9 @@ const char DinoGame::dinosaur = 'T';
 const char DinoGame::obstacle = 'O';
 
 int main() {
+    string name = "ppp";
+    Player player(name);
     DinoGame game;
-    game.runGame();
+    game.runGame(player);
     return 0;
 }
